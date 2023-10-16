d9825998ccfee5f2d758f94653b181744c9ae182
Liam DeWitt <mugzy550@gmail.com>
2023-10-16 19:31:59 -0400

    Build color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/Logo/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const navbar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize={"60px"} />
-      <Text>NavBar</Text>
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
     </HStack>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import theme from "./theme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </React.StrictMode>
 );
